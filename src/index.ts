b12fc239fdf889092346bfc56242e1757d1e549c
Ordluks <68952193+Ordluks@users.noreply.github.com>
2022-02-04 11:51:20 +0400

    1.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 import structureMaker from './structure'
+import { file as fileFabric } from './file'
+
+export const file = fileFabric
 export const structure = structureMaker
--- src/structure.ts
+++ src/structure.ts
@@ -4,15 +4,17 @@ import { makeFolder, Folder } from './folder'
 import { makeFile, File } from './file'
 
 
-const structure = (folders: Folder, root: string = process.cwd()) => forEach(Object.entries(folders), value => {
-	const [name, item] = value
-	if (is(File, item)) {
-		makeFile(root, name, item.content)
-	}
-	else {
-		const folderPath = makeFolder(root, name)
-		structure(item, folderPath)
-	}
-})
+const structure = (folders: Folder, root: string = process.cwd()) => {
+	forEach(Object.entries(folders), value => {
+		const [name, item] = value
+		if (is(File, item)) {
+			makeFile(root, name, item.content)
+		}
+		else {
+			const folderPath = makeFolder(root, name)
+			structure(item, folderPath)
+		}
+	})
+}
 
 export default structure
